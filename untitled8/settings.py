a5d20aa2e9d9c9f59c877c7f10d6d603f67efbe7
Your Name <silvia.homam@gmail.com>
2020-02-05 09:19:11 +0200

    Changed email backend to gmail smtp

--- untitled8/settings.py
+++ untitled8/settings.py
@@ -126,7 +126,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'email'
-EMAIL_HOST_PASSWORD = 'password'
+EMAIL_HOST_PASSWORD = 'pass'
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 
